78957fe7302a8e4c270af89f1eacf0f74576ebc1
lephuocthanh2000 <lephuocthanhnbk10h@gmail.com>
2022-06-23 14:51:43 +0700

    thanhle

---
+++ product.sql
@@ -0,0 +1,67 @@
+insert into products (name,unit_price,image,product_date,available,category_id,quantity,description,discount,view_count,special)
+values('Oppo A52 6GB-128GB',
+		5390000,
+		'oppo_A52.png',
+		'02/05/2020',
+		'1',
+		'1',
+		'100',
+		'
+		<h4>Đánh giá chi tiết Oppo A52 6GB-128GB</h4>
+		<p>&nbsp;</p>
+		<p>Cùng OPPO A52 tận hưởng cuộc sống, cho mỗi khoảnh khắc của bạn trở nên đáng nhớ hơn với màn hình Neo tuyệt đẹp, trải nghiệm mượt mà và bộ tứ camera đỉnh cao.</p>
+		<p><img alt="Oppo A52 6GB-128GB" src="https://img.fpt.shop/f_jpg/cmpr_10/https://fptshop.com.vn/Uploads/images/2015/Tin-Tuc/QuanLNH2/oppo-a52.jpg" /></p>
+		<p>&nbsp;</p>
+		<h4>Màn hình Neo hoàn toàn mới</h4>
+		<p>OPPO A52 sở hữu màn hình lớn 6,5 inch và độ phân giải Full HD+ 2400x1080 pixels sắc nét. Không chỉ xuất sắc ở chất lượng hiển thị, điểm nhấn màn hình OPPO A52 còn đến từ cách thiết kế. Các viền màn hình siêu mỏng và camera trước đặt khéo léo ngay trong tấm kính màn hình, tạo nên trải nghiệm xem ấn tượng nhất. OPPO gọi đây là thiết kế màn hình Neo, màn hình sẽ mang đến những điều khác biệt, đặc biệt là khi chơi game hay xem phim.</p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		',
+		'0.05',
+		'1',
+		'1')
+('Oppo A31 4GB-128GB',
+		4490000,
+		'oppo_A31.png',
+		'02/05/2020',
+		'1',
+		'1',
+		'100',
+		'
+		<h4>Đánh giá chi tiết Oppo A31 4GB-128GB</h4>
+		<p>&nbsp;</p>
+		<p>Với bộ nhớ trong cực lớn 128GB, viên pin dung lượng cao 4230 mAh và màn hình giọt nước 6,5 inch, OPPO A31 4GB-128GB mang đến cho bạn những trải nghiệm lớn hơn, giúp cuộc sống trở nên dễ dàng hơn.</p>
+		<p><img alt="Oppo A31 4GB-128GB" src="https://img.fpt.shop/f_jpg/cmpr_10/https://fptshop.com.vn/Uploads/images/2015/Tin-Tuc/QuanLNH2/oppo-a31-8.jpg" /></p>
+		<p>&nbsp;</p>
+		<h4>Một chiếc điện thoại chụp hình tốt trong mọi hoàn cảnh</h4>
+		<p>OPPO A31 sở hữu tới 3 camera sau, giúp bạn chụp ảnh xuất sắc trong mọi hoàn cảnh. Camera chính 12MP sẽ chụp ảnh sắc nét ở điều kiện thông thường; camera macro chuyên chụp cận cảnh những vật nhỏ ở khoảng cách cực gần; và cuối cùng là camera tạo độ sâu, hỗ trợ chụp ảnh chân dung xóa phông đầy chuyên nghiệp.</p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		',
+		'0.05',
+		'1',
+		'1')
+('Oppo A53 4GB-128GB',
+		4490000,
+		'oppo_A53.png',
+		'02/11/2020',
+		'1',
+		'1',
+		'50',
+		'
+		<h4>Đánh giá chi tiết Oppo A53 4GB-128GB</h4>
+		<p>&nbsp;</p>
+		<p>Mượt mà hơn bao giờ hết với màn hình 90Hz, sạc nhanh siêu tốc 18W và camera 16MP selfie tuyệt đẹp, OPPO A53 mang đến cho người dùng những bất ngờ thú vị trong tầm giá hấp dẫn.</p>
+		<p><img alt="Oppo A53 4GB-128GB" src="https://img.fpt.shop/f_jpg/cmpr_10/https://fptshop.com.vn/Uploads/images/2015/Tin-Tuc/QuanLNH2/oppo-a53.jpg" /></p>
+		<p>&nbsp;</p>
+		<h4>Cảm nhận sự mượt mà trên màn hình 90Hz của OPPO A53</h4>
+		<p>Màn hình OPPO A53 trở nên nổi bật với thiết kế viền mỏng dạng “đục lỗ” kiểu mới, mang đến trải nghiệm hiển thị “tràn viền” đẹp mắt hơn. Không chỉ vậy, với tần số quét lên tới 90Hz, cao gấp rưỡi so với màn hình smartphone thông thường, tất cả những gì bạn thấy hay những thao tác vuốt chạm trên OPPO A53 đều trở nên mượt mà hơn. Kể cả chơi game lẫn các tác vụ thường ngày, sự mượt mà của OPPO A53 mang đến cho bạn rất nhiều niềm vui khi sử dụng điện thoại.</p>
+		<p>&nbsp;</p>
+		',
+		'0.05',
+		'1',
+		'1')
+('Oppo Reno4 Pro',10190000,'oppo_reno4_pro.png','02/11/2020','1','1','50','OPPO Reno4 đẹp tựa như một kiệt tác trên tay bạn với kiểu dáng mỏng nhẹ thanh lịch và hoàn thiện dạng pha lê Reno Glow độc đáo. Công nghệ hoàn thiện Reno Glow mang đến một mặt lưng kính đặc biệt, bền hơn, sang trọng hơn và không bám bẩn, mồ hôi hay dấu vân tay. Để có được thiết kế xuất sắc, mỗi chiếc OPPO Reno4 đã phải trải qua nhiều quá trình điêu khắc tinh xảo, thay đổi cấu trúc vi mô của chất liệu kính. ','0.05','1','1')
+('OPPO A93 8GB-128GB',6360000,'oppo_A93.png','02/11/2020','1','1','50','OPPO Reno4 đẹp tựa như một kiệt tác trên tay bạn với kiểu dáng mỏng nhẹ thanh lịch và hoàn thiện dạng pha lê Reno Glow độc đáo. Công nghệ hoàn thiện Reno Glow mang đến một mặt lưng kính đặc biệt, bền hơn, sang trọng hơn và không bám bẩn, mồ hôi hay dấu vân tay. Để có được thiết kế xuất sắc, mỗi chiếc OPPO Reno4 đã phải trải qua nhiều quá trình điêu khắc tinh xảo, thay đổi cấu trúc vi mô của chất liệu kính. ','0.05','1','1'),
+('OPPO Reno4',7210000,'oppo_reno4.png','02/11/2020','1','1','50','OPPO Reno4 đẹp tựa như một kiệt tác trên tay bạn với kiểu dáng mỏng nhẹ thanh lịch và hoàn thiện dạng pha lê Reno Glow độc đáo. Công nghệ hoàn thiện Reno Glow mang đến một mặt lưng kính đặc biệt, bền hơn, sang trọng hơn và không bám bẩn, mồ hôi hay dấu vân tay. Để có được thiết kế xuất sắc, mỗi chiếc OPPO Reno4 đã phải trải qua nhiều quá trình điêu khắc tinh xảo, thay đổi cấu trúc vi mô của chất liệu kính. ','0.05','1','1'),
+('Samsung Galaxy Note 20 Ultra 5G',28490000,'samsung_note20_5G.png','02/11/2020','1','2','50','Samsung Galaxy Note20 Ultra 5G là chiếc điện thoại có được sự hoàn thiện tỉ mỉ nhất hiện nay. Sở hữu màn hình lớn tới 6,9 inch nhưng máy vẫn rất dễ dàng sử dụng, thao tác, cầm nắm nhờ phần khung viền cong mượt mà, liền lạc một cách hoàn hảo. Khung điện thoại làm từ kim loại sắc sảo, cắt kim cương sáng bóng và cả những đường phay xước phá cách.','0','1','0')
--- src/main/java/com/ThanhLe/thuongmaidientu/repository/ProductRepositoryImp.java
+++ src/main/java/com/ThanhLe/thuongmaidientu/repository/ProductRepositoryImp.java
@@ -8,12 +8,15 @@ import org.springframework.stereotype.Repository;
 
 
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 @Repository
 public class ProductRepositoryImp implements ProductRepository{
     @Autowired
     SessionFactory factory;
+
     @Override
     public Product findById(Integer id) {
         Session session=factory.getCurrentSession();
@@ -25,7 +28,7 @@ public class ProductRepositoryImp implements ProductRepository{
     public List<Product> findAll() {
         String hql="FROM Product";
         Session session=factory.getCurrentSession();
-        TypedQuery<Product> query=session.createQuery(hql, Product.class);
+        TypedQuery<Product> query=session.createQuery(hql,Product.class);
         List<Product> list=query.getResultList();
         return list;
     }
@@ -56,7 +59,7 @@ public class ProductRepositoryImp implements ProductRepository{
     public List<Product> findByCategoryId(Integer categoryId) {
         String hql="FROM Product p WHERE p.category.id=:cid";
         Session session=factory.getCurrentSession();
-        TypedQuery<Product> query=session.createQuery(hql, Product.class);
+        TypedQuery<Product> query=session.createQuery(hql,Product.class);
         query.setParameter("cid", categoryId);
         List<Product> list=query.getResultList();
         return list;
@@ -66,7 +69,7 @@ public class ProductRepositoryImp implements ProductRepository{
     public List<Product> findByKeywords(String keywords) {
         String hql="FROM Product p WHERE p.name LIKE :kw OR p.category.name LIKE :kw OR p.category.nameVN LIKE :kw";
         Session session=factory.getCurrentSession();
-        TypedQuery<Product> query=session.createQuery(hql, Product.class);
+        TypedQuery<Product> query=session.createQuery(hql,Product.class);
         query.setParameter("kw", "%"+keywords+"%");
         List<Product> list=query.getResultList();
         return list;
@@ -77,17 +80,16 @@ public class ProductRepositoryImp implements ProductRepository{
         String hql="FROM Product p WHERE p.id IN ("+ids+")";
 
         Session session=factory.getCurrentSession();
-        TypedQuery<Product> query=session.createQuery(hql, Product.class);
+        TypedQuery<Product> query=session.createQuery(hql,Product.class);
         List<Product> list=query.getResultList();
         return list;
     }
-
 
     @Override
     public List<Product> findBySpecial(Integer id) {
         Session session=factory.getCurrentSession();
         String hql="FROM Product p";
-        TypedQuery<Product> query= session.createQuery(hql,Product.class);
+        TypedQuery<Product> query=session.createQuery(hql,Product.class);
         switch (id) {
             case 0://mới
                 hql="FROM Product p ORDER BY p.productDate DESC";
@@ -115,6 +117,4 @@ public class ProductRepositoryImp implements ProductRepository{
         List<Product> list=query.getResultList();
         return list;
     }
-
-
 }
